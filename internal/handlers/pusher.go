78a704e6feb837a3cb43e48fec443ce509f5bfec
William Tran <wtran29@gmail.com>
2023-07-16 19:24:27 -0700

    included sending private message function

--- internal/handlers/pusher.go
+++ internal/handlers/pusher.go
@@ -3,6 +3,7 @@
 package handlers
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -35,4 +36,16 @@ func (repo *DBRepo) PusherAuth(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	_, _ = w.Write(resp)
+}
+
+// SendPrivateMessage is sample code for sending to private channel
+func (repo *DBRepo) SendPrivateMessage(w http.ResponseWriter, r *http.Request) {
+	msg := r.URL.Query().Get("msg")
+	id := r.URL.Query().Get("id")
+
+	data := make(map[string]string)
+	data["message"] = msg
+
+	_ = repo.App.WsClient.Trigger(fmt.Sprintf("private-channel-%s", id), "private-message", data)
+
 }
